9637853a26d9da29e2718975a594c09a9076852d
27dimon27 <dmnagdasev@gmail.com>
2026-03-16 16:57:59 +0300

    [fix] добавил второго юзера без заметок

--- migrations/002_seed_data.sql
+++ migrations/002_seed_data.sql
@@ -1,9 +1,17 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-INSERT INTO accounts (id, username, password, token_version, created_at, updated_at) 
-VALUES (
+INSERT INTO accounts (id, username, password, token_version, created_at, updated_at) VALUES
+(
     '11111111-1111-1111-1111-111111111111',
     'testuser',
+    crypt('Password123', gen_salt('bf'))::bytea,
+    1,
+    NOW() - INTERVAL '1 month',
+    NOW()
+),
+(
+    '22222222-2222-2222-2222-222222222222',
+    'testuser2',
     crypt('Password123', gen_salt('bf'))::bytea,
     1,
     NOW() - INTERVAL '1 month',
