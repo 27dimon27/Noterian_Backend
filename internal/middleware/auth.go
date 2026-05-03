fb687f8886222b6c3bb31d7c603dedb0f832816c
27dimon27 <dmnagdasev@gmail.com>
2026-05-03 21:55:51 +0300

    Revert "[fix] фикс мидлвари для ws"
    This reverts commit 2ea09d6772d4b1cc651761bd4056f0d9d46d0406.

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -36,30 +36,3 @@ func Auth(next http.Handler, jwtConfig config.JWTConfig) http.Handler {
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
-
-func WebSocketAuth(jwtConfig config.JWTConfig) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			cookieJWT, err := r.Cookie(jwtConfig.CookieName)
-			if err != nil {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
-			}
-
-			tokenPayload, err := jwt.ValidateToken(cookieJWT.Value, jwtConfig.Secret)
-			if err != nil {
-				http.Error(w, "Invalid token", http.StatusUnauthorized)
-				return
-			}
-
-			userUUID, err := uuid.Parse(tokenPayload.UserID)
-			if err != nil {
-				http.Error(w, "Invalid user ID", http.StatusUnauthorized)
-				return
-			}
-
-			ctx := context.WithValue(r.Context(), types.UserIDKey, userUUID)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
-}
